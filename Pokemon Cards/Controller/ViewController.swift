edffdf6e14d29ba7ec6788cf19a75a3a6d52df3d
Kaan TOKSOY <toksoykaan@gmail.com>
2022-03-06 23:22:21 +0300

    Done:
    #Swipe left right functionality and UI
    #Card view UI improvement
    
    To do :
    
    #API code simplicity
    #Loading indicator needed
    #Device rotate design -> viewdidlayoutsubviews
    #Unit testing

--- Pokemon Cards/Controller/ViewController.swift
+++ Pokemon Cards/Controller/ViewController.swift
@@ -16,25 +16,84 @@ class ViewController: UIViewController {
     @IBOutlet weak var attackOutlet: UILabel!
     @IBOutlet weak var defenceOutlet: UILabel!
     
+    @IBOutlet weak var viewOutlet: UIView!
+    
     @IBOutlet weak var imageOutlet: UIImageView!
+    
+    
+    var pokemonNumber = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        modelManager.getPokemons(pokemonNumber: 1)
-        getParameters()
-        modelManager.imageDownloader(from: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")!) { image in
-            self.imageOutlet.image = image
-        }
+        viewOutlet.layer.cornerRadius = 50
+        
+        
+        modelManager.getPokemons(pokemonNumber: pokemonNumber)
+        getSetParameters()
+
+        
+        
+        //MARK: Declaring card gesture recognizer to keep track of user input
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+            swipeLeft.direction = .left
+            self.view.addGestureRecognizer(swipeLeft)
+                
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+            swipeRight.direction = .right
+            self.view.addGestureRecognizer(swipeRight)
+        
     }
     
-    func getParameters(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [self] in
-            nameOutlet.text = modelManager.name
+    
+    func getSetParameters(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [self] in
+
+            nameOutlet.text = modelManager.name?.capitalized
             hpOutlet.text = modelManager.hp
             attackOutlet.text = modelManager.attack
             defenceOutlet.text = modelManager.def
+            
+            modelManager.imageDownloader(from: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(pokemonNumber).png")!) { image in
+                self.imageOutlet.image = image
+            }
         }
-}
+    }
+    
+    
+    
+    
+    //MARK: Here we declare the rules of swiping left and right, we will call for new card data as well as flipping animation
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) {
+       if gesture.direction == .right {
+           
+               if pokemonNumber != 1{
+               pokemonNumber -= 1
+               modelManager.getPokemons(pokemonNumber: pokemonNumber)
+               getSetParameters()
+               }else{
+                   pokemonNumber = 20
+                   modelManager.getPokemons(pokemonNumber: pokemonNumber)
+                   getSetParameters()
+               }
+           
+           UIView.transition(with: viewOutlet, duration: 0.5, options: .transitionFlipFromTop, animations: nil, completion: nil)
+       }
+       else if gesture.direction == .left {
+        
+               if pokemonNumber != 20{
+               pokemonNumber += 1
+               modelManager.getPokemons(pokemonNumber: pokemonNumber)
+               getSetParameters()
+               }else{
+                   pokemonNumber = 1
+                   modelManager.getPokemons(pokemonNumber: pokemonNumber)
+                   getSetParameters()
+               }
+           
+           UIView.transition(with: viewOutlet, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+       }
+    }
 
+    
 }
